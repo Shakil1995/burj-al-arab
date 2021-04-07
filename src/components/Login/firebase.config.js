7c0b79d3ebe9b7f834510591d2ec64e2d3b716ae
Nurul Amin Shakil <na.shakil1995@gmail.com>
2021-04-08 03:14:14 +0600

    login route done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBdfP3fSsrA5YQiTraDV73cGQ2Wp-SwC9s",
+    authDomain: "burj-al-arab00.firebaseapp.com",
+    projectId: "burj-al-arab00",
+    storageBucket: "burj-al-arab00.appspot.com",
+    messagingSenderId: "860746036452",
+    appId: "1:860746036452:web:a88b72a24a2e013f8ba8f1"
+  };
+  export default firebaseConfig;
